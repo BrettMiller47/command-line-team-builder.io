4399caacdfb503c0b2a1bb72aaf00d44b45d9082
Brett Miller <brett.miller47@yahoo.com>
2022-08-31 22:36:35 -0400

    index.html: Completed the index.html final appearance.  Dvided this completed html code ito two seinctions: 1) startingHtmlContent -- contains bootstrap css styling, <script> src, and root elements
    2) dynamic HTML content
    -- each Employees sub-class has a method called getHtmlContent. This is the html for each card
    
    team.js:
    1) pseudocoded the remaining necessary code
    2) added functions addManager(), addEngineer(), and addIntern()
    -- Successfully retrieving user inputs
    -- Unsuccessfully getting html content
    
    Outstanding Items:
    1) address the unsuccessful html content gathering mentioned above
    2) add appendHtml() method to each sub-Class
    3) call the appendHtml() method in addManager(), addEngineer(), and addIntern()

--- employees.js
+++ employees.js
@@ -12,6 +12,26 @@ class Manager extends Employee {
         super(name, id, email);
         this.officeNum = officeNum;
     }
+    
+    /**
+     * gets the HTML content that is appended to the DOM
+     */
+    getHtmlContent() {
+        let htmlContent = `
+        <section class="row-fluid p-0 d-flex justify-content-center">
+            <div class="card col-3 m-3 p-2 bg-dark text-light"  style="width: 32rem">
+                <h1 class="p-2">${this.name}</h1>
+                <h1 class="p-2">Manager</h1>
+                <ul class="list-group list-group-flush p-2">
+                    <li class="list-group-item">ID: ${this.id}</li>
+                    <li class="list-group-item">Email: <a href="${this.email}">${this.email}</a></li>
+                    <li class="list-group-item">Office: ${this.officeNum}</li>
+                </ul>
+            </div>
+        </section>
+        `;
+        return htmlContent;
+    }
 }
 
 
@@ -20,6 +40,25 @@ class Engineer extends Employee {
         super(name, id, email);
         this.githubName = githubName;
     }
+
+    /**
+     * gets the HTML content that is appended to the DOM
+     */
+    getHtmlContent() {
+        let htmlContent = `
+        <div class="card col-3 m-3 p-2 bg-primary text-light" style="width: 32rem">
+            <h1 class="p-2">${this.name}</h1>
+            <h1 class="p-2">Engineer</h1>
+            <ul class="list-group list-group-flush p-2">
+                <li class="list-group-item">ID: ${this.id}</li>
+                <li class="list-group-item">Email: <a href="${this.email}">${this.email}</a></li>
+                <li class="list-group-item">Office:</li>
+                <li class="list-group-item">GitHub: ${this.githubName}<a href="https:github.com/${this.githubName}" target="__blank"></a></li>
+            </ul>
+        </div>
+        `;
+        return htmlContent;
+    }
 }
 
 
@@ -27,6 +66,25 @@ class Intern extends Employee {
     constructor(name, id, email, school) {
         super(name, id, email);
         this.school = school;
+    }
+
+    /**
+     * gets the HTML content that is appended to the DOM
+     */
+    getHtmlContent() {
+        let htmlContent = `
+        <div class="card col-3 m-3 p-2 bg-primary text-light" style="width: 32rem">
+            <h1 class="p-2">${this.name}</h1>
+            <h1 class="p-2">Intern</h1>
+            <ul class="list-group list-group-flush p-2">
+                <li class="list-group-item">ID: ${this.id}</li>
+                <li class="list-group-item">Email: <a href="${this.email}">${this.email}</a></li>
+                <li class="list-group-item">Office:</li>
+                <li class="list-group-item">School: ${this.school}</li>
+            </ul>
+        </div>
+        `;
+        return htmlContent;
     }
 }
 
--- team.js
+++ team.js
@@ -1,2 +1,143 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
+const { start } = require("repl");
+const { Manager } = require("./employees");
+const { Engineer } = require("./employees");
+const { Intern } = require("./employees");
+
+const startingHtmlContent = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <title>Team</title>
+</head>
+<body class="container-fluid m-0 p-0">
+    <header id="rootElManager>" class="row col-12 m-0 p-0 bg-dark text-light text-center">
+        <h1>My Team</h1>
+    </header>
+    <!-- header is rootEl for Manager card -->
+    <!-- Manager card is rootEl for other team members -->
+    <section id="rootElTeam" class="row p-0 d-flex justify-content-center">
+    </section>
+    </main>    
+</body>
+<script src="team.js"></script>
+</html>
+`;
+
+// Create index.html with starting template
+fs.writeFile('index.html', startingHtmlContent, (err) => {
+    if (err) { throw err };
+});
+// Add the manager to the DOM
+addManager();
+// While adding members
+    // prompt user to decide Engineer or Intern
+    // if engineer...
+        // add engineer tp the DOM
+    // if Intern...
+        // add intern to the DOM
+
+// Get Manager info and create their HTML card
+function addManager() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "Enter manager's name:",
+                name: "name"
+            },
+            {
+                type: "input",
+                message: "Enter manager's ID number:",
+                name: "id"
+            },
+            {
+                type: "input",
+                message: "Enter manager's email:",
+                name: "email"
+            },
+            {
+                type: "input",
+                message: "Enter manager's office number:",
+                name: "officeNum"
+            },
+        ]).then((res) => 
+            new Manager(res.name, res.id, res.email, res.officeNum)
+        ).then((manager) => {
+            let htmlContent = manager.getHtmlContent();
+            console.log(htmlContent);
+        });
+        
+}
+
+// Get Engineer info and create their HTML card
+function getEngineerInfo() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "Enter engineer's name:",
+                name: "name"
+            },
+            {
+                type: "input",
+                message: "Enter engineer's ID number:",
+                name: "id"
+            },
+            {
+                type: "input",
+                message: "Enter engineer's email:",
+                name: "email"
+            },
+            {
+                type: "input",
+                message: "Enter engineer's GitHub profile name:",
+                name: "githubName"
+            },
+        ]).then((res) => 
+            new Engineer(res.name, res.id, res.email, res.githubName)
+        ).then((engineer) => {
+            let htmlContent = engineer.getHtmlContent();
+            console.log(htmlContent);
+        });
+}
+
+// Get Intern info and create their HTML card
+function getInternInfo() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "Enter intern's name:",
+                name: "name"
+            },
+            {
+                type: "input",
+                message: "Enter intern's ID number:",
+                name: "id"
+            },
+            {
+                type: "input",
+                message: "Enter intern's email:",
+                name: "email"
+            },
+            {
+                type: "input",
+                message: "Enter intern's school:",
+                name: "officeNum"
+            },
+        ]).then((res) => 
+            new Intern(res.name, res.id, res.email, res.school)
+        ).then((intern) => {
+            let htmlContent = intern.getHtmlContent();
+            console.log(htmlContent);
+        });
+}
